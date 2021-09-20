7341e1c160759b42f52ab563df6342b6de534aaf
Konrad <konradrydzak@gmail.com>
2021-09-20 23:25:36 +0200

    database tables and columns naming convention changed

--- .sql/initial_migration.sql
+++ .sql/initial_migration.sql
@@ -3,29 +3,29 @@ drop table if exists orders cascade;
 drop table if exists ordered_items cascade;
 
 create table menu_items (
-	"menu_id" serial NOT NULL PRIMARY KEY,
+	"id" serial NOT NULL PRIMARY KEY,
 	"category" varchar(100) NOT NULL,
 	"name" varchar(100) NOT NULL,
 	"price" numeric(6, 2) NOT NULL
 );
 
 create table orders (
-	"order_id" serial NOT NULL PRIMARY KEY,
+	"id" serial NOT NULL PRIMARY KEY,
 	"total_price" numeric(6, 2) NOT NULL,
 	"comments" text,
 	"email" varchar(100)
 );
 
 create table ordered_items (
-	"ordered_item_id" serial NOT NULL PRIMARY KEY,
-	"menu_id" int NOT NULL,
+	"id" serial NOT NULL PRIMARY KEY,
+	"menu_item_id" int NOT NULL,
 	"order_id" int NOT NULL,
 	"quantity" int NOT NULL,
 	"unit_price" numeric(6, 2) NOT NULL,
-	foreign key ("menu_id")
-		references menu_items ("menu_id") on delete cascade,
+	foreign key ("menu_item_id")
+		references menu_items ("id") on delete cascade,
 	foreign key ("order_id")
-		references orders ("order_id") on delete cascade
+		references orders ("id") on delete cascade
 );
 
 INSERT INTO public."menu_items" ("name", "category", "price") VALUES ('Margheritta', 'Pizza', 20);
